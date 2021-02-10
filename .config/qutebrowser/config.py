4c6fa4226c66ba862a94c74d846837ab91c8fc0c
Ace_BCB <benjamincb@hotmail.dk>
2021-02-10 22:33:10 +0100

    qutebrowser does not load gui config anymore

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,7 +2,7 @@
 config.set("colors.webpage.darkmode.enabled",True)
 
 #fonts
-c.fonts.default_family = "fira code"
+# c.fonts.default_family = "fira code"
 c.fonts.default_size = "16pt"
 c.fonts.hints = "22pt"
 
@@ -13,3 +13,5 @@ config.bind(">", "tab-next")
 #zoom
 c.zoom.default = "150%"
 c.zoom.levels = ["25%", "33%", "50%", "67%", "75%", "90%", "100%", "110%", "120%", "130%", "140%", "150%", "160%", "170%", "180%", "190%", "200%", "250%", "300%", "400%", "500%"]
+
+config.load_autoconfig(False)
