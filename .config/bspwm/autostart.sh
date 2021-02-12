98c86a2ef801bf2837b3940892c9fa07a68e693d
Ace_BCB <benjamincb@hotmail.dk>
2021-02-12 21:00:38 +0100

    fixed wallpaper issues

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -12,7 +12,7 @@ function run {
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-/home/bcb/git/scripts/screen_rotation.sh &
+/home/bcb/git/scripts/screen_rotation.sh 
 
 #$HOME/.config/polybar/launch.sh &
 
@@ -21,21 +21,26 @@ function run {
 
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
-feh --bg-fill ~/Pictures/wallpapers/foggy_forest.jpg &
+# feh --bg-fill ~/Pictures/foggy_forest_1.jpg &
 #feh --randomize --bg-fill ~/Képek/*
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
+
+# im am root picture
+# https://imgur.com/a/Ix8QBfl
+nitrogen --head=0 --set-auto ~/Pictures/i_am_root.jpg 
+nitrogen --head=1 --set-auto ~/Pictures/explosion_vertical.png 
 
 dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 #nitrogen --restore &
-conky -c ~/.config/conky/inf-crimson.conkyrc &
+# conky -c ~/.config/conky/inf-crimson.conkyrc &
 run nm-applet &
-run pamac-tray &
+# run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
-blueberry-tray &
+# blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
