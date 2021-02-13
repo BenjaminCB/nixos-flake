686d5b731683cc922daf3d1138183e9bd7e66e3e
Ace_BCB <benjamincb@hotmail.dk>
2021-02-13 22:41:59 +0100

    changed image previewer back to w3m since scaling is better

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -7,11 +7,6 @@ function run {
   fi
 }
 
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 /home/bcb/git/scripts/screen_rotation.sh 
 
 #$HOME/.config/polybar/launch.sh &
