3e39991e7d8ca08f868e80cf0f9efdd98b19dc18
Ace_BCB <benjamincb@hotmail.dk>
2021-03-07 17:04:35 +0100

    is now split into laptop and desktop parts

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -7,47 +7,29 @@ function run {
   fi
 }
 
-/home/bcb/git/scripts/screen_rotation.sh 
+if [[ $(hostname) == 'arco-desktop' ]]; then
+    /home/bcb/git/scripts/screen_rotation.sh 
+fi
 
 #$HOME/.config/polybar/launch.sh &
-
-#change your keyboard if you need it
-#setxkbmap -layout be
-
-#Some ways to set your wallpaper besides variety or nitrogen
-#feh --bg-scale ~/.config/bspwm/wall.png &
-# feh --bg-fill ~/Pictures/foggy_forest_1.jpg &
-#feh --randomize --bg-fill ~/Képek/*
-#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
 # im am root picture
 # https://imgur.com/a/Ix8QBfl
 nitrogen --head=0 --set-zoom-fill ~/git/wallpapers/horizontal/Shiomiya_Shiori_Holding_Shy_C_Programming_language_wp_gruvbox.png
 nitrogen --head=1 --set-auto ~/git/wallpapers/vertical/anime_book_computer_grevbox_neofetch_vim.png
 
-# dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
-#nitrogen --restore &
-# conky -c ~/.config/conky/inf-crimson.conkyrc &
-run nm-applet &
-# run pamac-tray &
-run xfce4-power-manager &
+if [[ $(hostname) == 'arco-desktop' ]]; then
+    run nm-applet &
+    run xfce4-power-manager &
+    picom --config $HOME/.config/bspwm/picom.conf &
+    /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+    run volumeicon &
+else
+    batsignal -b &
+fi
 numlockx on &
-# blueberry-tray &
-picom --config $HOME/.config/bspwm/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
-run volumeicon &
 redshift &
-#nitrogen --restore &
-#run caffeine &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run discord &
-#run spotify &
-#run atom &
