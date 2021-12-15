d5d77cd19b34acacf4125301f1135c86ddfe19af
BenjaminCB <benjamincb@hotmail.dk>
2021-12-15 16:41:17 +0100

    rofi appears on monitor with focused window

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,4 +41,4 @@ alias ds='node ~/git/scripts/danishLetterParser/danishSearch.mjs'
 alias lock='xsecurelock'
 
 cat ~/.config/kitty/ascii_cat.txt
-alias mon2cam="deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts"
+alias montocam="deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts"
