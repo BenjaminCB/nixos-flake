630f66e86cf6f08850b982f253274bf96c4733ba
BenjaminCB <benjamincb@hotmail.dk>
2022-08-18 13:18:43 +0200

    add yamlls

--- .config/nvim/lua/bcb/lsp.lua
+++ .config/nvim/lua/bcb/lsp.lua
@@ -39,6 +39,7 @@ local servers = { "tsserver"
                 , "elmls"
                 , "purescriptls"
                 , "sumneko_lua"
+                , "yamlls"
                 }
 
 for _, lsp in ipairs(servers) do
