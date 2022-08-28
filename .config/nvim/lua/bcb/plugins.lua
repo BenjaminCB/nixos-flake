36cf8b9a9106b8da2d9b64e6598071b8a4216e8e
BenjaminCB <benjamincb@hotmail.dk>
2022-08-28 19:14:35 +0200

    hopefully add bootstrapping for packer

--- .config/nvim/lua/bcb/plugins.lua
+++ .config/nvim/lua/bcb/plugins.lua
@@ -1,82 +1,91 @@
-return require('packer').startup {
-    function(use)
-        use 'wbthomason/packer.nvim'
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+end
 
-        use {
-            'neovim/nvim-lspconfig',
-            config = [[require('bcb.lsp')]]
-        }
-        use {
-            'hrsh7th/nvim-compe',
-            config = [[require('bcb.compe')]]
-        }
-        use {
-            'nvim-telescope/telescope.nvim',
-            requires = {
-                {'nvim-lua/popup.nvim'},
-                {'nvim-lua/plenary.nvim'}
-            },
-            config = [[require('bcb.telescope')]]
-        }
-        use 'nvim-telescope/telescope-fzy-native.nvim'
-        use {'lervag/vimtex', ft = {'tex'}}
-        use 'SirVer/ultisnips'
-        use 'tpope/vim-fugitive'
-        use 'airblade/vim-gitgutter'
-        -- use {
-        --     'TimUntersberger/neogit',
-        --     requires = {
-        --         { 'nvim-lua/plenary.nvim' },
-        --         { 'sindrets/diffview.nvim' }
-        --     }
-        -- }
-        use {
-            'dense-analysis/ale',
-            ft = {'sh', 'haskell', 'bash', 'lua', 'python',}
-        }
-        use {
-            'nvim-treesitter/nvim-treesitter',
-            -- config = require('nvim-treesitter.configs').setup{highlight = { enable = true }}
-        }
-        -- use 'dbeniamine/cheat.sh-vim'
-        use 'tpope/vim-surround'
-        use 'tpope/vim-commentary'
-        use 'chrisbra/Colorizer'
-        use 'rstacruz/vim-closer'
-        -- use 'junegunn/vim-easy-align'
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
 
-        use {
-            'iamcco/markdown-preview.nvim',
-            run = 'cd app && yarn install',
-            -- run = '{ -> mkdp#util#install() }',
-            ft = {'markdown'}
-        }
+    use {
+        'neovim/nvim-lspconfig',
+        config = [[require('bcb.lsp')]]
+    }
+    use {
+        'hrsh7th/nvim-compe',
+        config = [[require('bcb.compe')]]
+    }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+            {'nvim-lua/popup.nvim'},
+            {'nvim-lua/plenary.nvim'}
+        },
+        config = [[require('bcb.telescope')]]
+    }
+    use 'nvim-telescope/telescope-fzy-native.nvim'
+    use {'lervag/vimtex', ft = {'tex'}}
+    use 'SirVer/ultisnips'
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
+    -- use {
+    --     'TimUntersberger/neogit',
+    --     requires = {
+    --         { 'nvim-lua/plenary.nvim' },
+    --         { 'sindrets/diffview.nvim' }
+    --     }
+    -- }
+    use {
+        'dense-analysis/ale',
+        ft = {'sh', 'haskell', 'bash', 'lua', 'python',}
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        -- config = require('nvim-treesitter.configs').setup{highlight = { enable = true }}
+    }
+    -- use 'dbeniamine/cheat.sh-vim'
+    use 'tpope/vim-surround'
+    use 'tpope/vim-commentary'
+    use 'chrisbra/Colorizer'
+    use 'rstacruz/vim-closer'
+    -- use 'junegunn/vim-easy-align'
 
-        use {
-          "folke/trouble.nvim",
-          requires = {
-              { "kyazdani42/nvim-web-devicons" },
-              { "folke/lsp-colors.nvim" }
-          },
-          config = function()
-            require("trouble").setup {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-            }
-          end
-        }
+    use {
+        'iamcco/markdown-preview.nvim',
+        run = 'cd app && yarn install',
+        -- run = '{ -> mkdp#util#install() }',
+        ft = {'markdown'}
+    }
 
-        use 'rktjmp/lush.nvim'
-        use 'morhetz/gruvbox'
-        use 'sjl/badwolf'
-        use 'hugolgst/vimsence'
-        use 'NLKNguyen/papercolor-theme'
-        use {
-            'hoob3rt/lualine.nvim',
-            requires = {'kyazdani42/nvim-web-devicons', opt = true},
-            config = [[require('bcb.lualine')]]
+    use {
+      "folke/trouble.nvim",
+      requires = {
+          { "kyazdani42/nvim-web-devicons" },
+          { "folke/lsp-colors.nvim" }
+      },
+      config = function()
+        require("trouble").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
         }
+      end
+    }
 
-        use 'sheerun/vim-polyglot'
+    use 'rktjmp/lush.nvim'
+    use 'morhetz/gruvbox'
+    use 'sjl/badwolf'
+    use 'hugolgst/vimsence'
+    use 'NLKNguyen/papercolor-theme'
+    use {
+        'hoob3rt/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true},
+        config = [[require('bcb.lualine')]]
+    }
+
+    use 'sheerun/vim-polyglot'
+
+    if packer_bootstrap then
+        require('packer').sync()
     end
-}
+end)
