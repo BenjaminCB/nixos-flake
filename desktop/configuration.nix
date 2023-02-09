6419b0e464e4e070d62772d2258ba0d156cff4ce
bcb <benjamincb@hotmail.dk>
2023-02-09 19:30:21 +0100

    monitor setup

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -11,5 +11,12 @@
       ../configuration.nix
     ];
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+    displayManager.setupCommands = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --output DVI-D-1 --mode 1920x1080 --pos 3000x536 --rotate normal --output DVI-D-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-1 --primary --mode 1920x1080 --pos 1080x331 --rotate normal
+    '';
+  };
+
+
 }
