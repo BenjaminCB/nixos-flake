7a0fff17e082a5ef0b22aa29989fd2ea15bc5bc5
bcb <benjamincb@hotmail.dk>
2023-02-10 20:49:19 +0100

    try to get spelling files in neovim

--- home.nix
+++ home.nix
@@ -83,4 +83,13 @@
         };
     };
 
+    home.file."${config.xdg.configHome}/nvim/spell/da.utf-8.spl".source = builtins.fetchurl {
+        url = "http://ftp.vim.org/vim/runtime/spell/da.utf-8.spl";
+        sha256 = "0cl9q1ln7y4ihbpgawl3rc86zw8xynq9hg4hl8913dbmpcl2nslj";
+    };
+
+    home.file."${config.xdg.configHome}/nvim/spell/da.utf-8.sug".source = builtins.fetchurl {
+        url = "http://ftp.vim.org/vim/runtime/spell/da.utf-8.sug";
+        sha256 = "1pdnp0hq3yll65z6rlmq0l6axvn5450jw5y081vyb4x5850czdxm";
+    };
 }
