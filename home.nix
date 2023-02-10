761bd1b1f5c59a2010bbce9a5713fb53608eed31
bcb <benjamincb@hotmail.dk>
2023-02-10 23:50:28 +0100

    preview images in ranger

--- home.nix
+++ home.nix
@@ -28,7 +28,19 @@
         ncdu
         pandoc
         pcmanfm
-        ranger
+        (ranger.overrideAttrs (r: {
+          preConfigure = r.preConfigure + ''
+            # Specify path to Überzug
+            substituteInPlace ranger/ext/img_display.py \
+              --replace "Popen(['ueberzug'" \
+                        "Popen(['${pkgs.ueberzug}/bin/ueberzug'"
+
+            # Use Überzug as the default method
+            substituteInPlace ranger/config/rc.conf \
+              --replace 'set preview_images_method w3m' \
+                        'set preview_images_method ueberzug'
+          '';
+        }))
         ripgrep
         rofi
         spotify
