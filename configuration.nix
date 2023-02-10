10a246bc4ecf5ca742148d02c3f8f444e11020bf
bcb <benjamincb@hotmail.dk>
2023-02-10 14:34:08 +0100

    fix some indentation

--- configuration.nix
+++ configuration.nix
@@ -5,92 +5,91 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
+    imports = [ # Include the results of the hardware scan.
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Copenhagen";
+    # Set your time zone.
+    time.timeZone = "Europe/Copenhagen";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_DK.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_DK.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "da_DK.UTF-8";
-    LC_IDENTIFICATION = "da_DK.UTF-8";
-    LC_MEASUREMENT = "da_DK.UTF-8";
-    LC_MONETARY = "da_DK.UTF-8";
-    LC_NAME = "da_DK.UTF-8";
-    LC_NUMERIC = "da_DK.UTF-8";
-    LC_PAPER = "da_DK.UTF-8";
-    LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "da_DK.UTF-8";
-  };
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "da_DK.UTF-8";
+        LC_IDENTIFICATION = "da_DK.UTF-8";
+        LC_MEASUREMENT = "da_DK.UTF-8";
+        LC_MONETARY = "da_DK.UTF-8";
+        LC_NAME = "da_DK.UTF-8";
+        LC_NUMERIC = "da_DK.UTF-8";
+        LC_PAPER = "da_DK.UTF-8";
+        LC_TELEPHONE = "da_DK.UTF-8";
+        LC_TIME = "da_DK.UTF-8";
+    };
 
-  console = {
-    font = "latarcyrheb-sun32";
-    keyMap = "us";
-  };
+    console = {
+        font = "latarcyrheb-sun32";
+        keyMap = "us";
+    };
 
-  # Configure keymap in X11
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    xkbVariant = "";
-    libinput.enable = true;
-    displayManager.lightdm.greeters.slick.enable = true;
-    displayManager.defaultSession = "none+awesome";
-    windowManager.awesome.enable = true;
-    desktopManager.xfce.enable = true;
-  };
+    # Configure keymap in X11
+    services.xserver = {
+        enable = true;
+        layout = "us";
+        xkbVariant = "";
+        libinput.enable = true;
+        displayManager.lightdm.greeters.slick.enable = true;
+        displayManager.defaultSession = "none+awesome";
+        windowManager.awesome.enable = true;
+        desktopManager.xfce.enable = true;
+    };
 
-  virtualisation.docker.enable = true;
-  location.provider = "geoclue2";
-  services.redshift.enable = true;
+    virtualisation.docker.enable = true;
+    location.provider = "geoclue2";
+    services.redshift.enable = true;
 
-  # Enable sound
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+    # Enable sound
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.bcb = {
-    isNormalUser = true;
-    description = "bcb";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.bcb = {
+        isNormalUser = true;
+        description = "bcb";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [];
+    };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    firefox
-    discord
-    git
-  ];
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim
+        wget
+        firefox
+        discord
+        git
+    ];
 
-  environment.sessionVariables = rec {
-    NNN_FIFO="/tmp/nnn.fifo";
-  };
+    environment.sessionVariables = rec {
+        NNN_FIFO="/tmp/nnn.fifo";
+    };
 
     xdg.mime = {
         enable = true;
@@ -99,58 +98,58 @@
         };
     };
 
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-  ];
+    fonts.fonts = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        proggyfonts
+    ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-  # List services that you want to enable:
+    # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  nixpkgs.overlays = [
-    (self: super: {
-      discord = super.discord.overrideAttrs (
-        _: { src = builtins.fetchTarball {
-          url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-          sha256 = "087p8z538cyfa9phd4nvzjrvx4s9952jz1azb2k8g6pggh1vxwm8";
-        }; }
-      );
-    })
-  ];
+    nixpkgs.overlays = [
+        (self: super: {
+            discord = super.discord.overrideAttrs (
+                _: { src = builtins.fetchTarball {
+                    url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+                    sha256 = "087p8z538cyfa9phd4nvzjrvx4s9952jz1azb2k8g6pggh1vxwm8";
+                }; }
+            );
+        })
+    ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "22.11"; # Did you read the comment?
 
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = "experimental-features = nix-command flakes";
-  };
+    nix = {
+        package = pkgs.nixFlakes;
+        extraOptions = "experimental-features = nix-command flakes";
+    };
 }
