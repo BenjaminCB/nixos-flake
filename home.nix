155e8a06dd65491416249182a46dbe36cf3f18c9
bcb <benjamincb@hotmail.dk>
2023-02-12 00:12:41 +0100

    more mime types

--- home.nix
+++ home.nix
@@ -97,18 +97,22 @@ in
     xdg.mimeApps = {
         enable = true;
         defaultApplications = {
-            "application/pdf" = "org.pwmt.zathura.desktop";
-            "image/png" = "sxiv.desktop";
-            "image/jpeg" = "sxiv.desktop";
-            "text/html" = "firefox.desktop";
-            "application/x-extension-htm" = "firefox.desktop";
-            "application/x-extension-html" = "firefox.desktop";
+            "application/pdf"               = "org.pwmt.zathura.desktop";
+            "image/png"                     = "sxiv.desktop";
+            "image/jpeg"                    = "sxiv.desktop";
+            "text/html"                     = "firefox.desktop";
+            "application/x-extension-htm"   = "firefox.desktop";
+            "application/x-extension-html"  = "firefox.desktop";
             "application/x-extension-shtml" = "firefox.desktop";
-            "application/xhtml+xml" = "firefox.desktop";
+            "application/xhtml+xml"         = "firefox.desktop";
             "application/x-extension-xhtml" = "firefox.desktop";
-            "application/x-extension-xht" = "firefox.desktop";
-            "application/x-www-browser" = "firefox.desktop";
-            "x-www-browser" = "firefox.desktop";
+            "application/x-extension-xht"   = "firefox.desktop";
+            "application/x-www-browser"     = "firefox.desktop";
+            "x-www-browser"                 = "firefox.desktop";
+            "x-scheme-handler/http"         = "firefox.desktop";
+            "x-scheme-handler/https"        = "firefox.desktop";
+            "x-scheme-handler/about"        = "firefox.desktop";
+            "x-scheme-handler/unknown"      = "firefox.desktop";
         };
     };
 }
