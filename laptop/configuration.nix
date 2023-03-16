ea25ad9d86f437ab6491e8d5517a52ae7bfff7c7
bcb <benjamincb@hotmail.dk>
2023-03-16 22:38:53 +0100

    Revert "maybe add acpi only to laptop"
    This reverts commit 9af865891b811f374407bae93c38a14bbc72e13f.

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -8,7 +8,6 @@
     imports = [ # Include the results of the hardware scan.
         ./hardware-configuration.nix
         ../configuration.nix
-        ./home.nix
     ];
 
     services.xserver.displayManager = {
