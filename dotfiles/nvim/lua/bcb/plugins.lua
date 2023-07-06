734f6174cd5b4ac3faded18b62d4f9f08a85ff71
bcb <benjamincb@hotmail.dk>
2023-07-06 21:31:51 +0200

    add coqtail

--- dotfiles/nvim/lua/bcb/plugins.lua
+++ dotfiles/nvim/lua/bcb/plugins.lua
@@ -88,6 +88,8 @@ return require('packer').startup(function(use)
 
     use 'ThePrimeagen/harpoon'
 
+    use 'whonore/Coqtail'
+
     use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }})
 
     if packer_bootstrap then
