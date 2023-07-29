0c071783bf869f3c1f17bb2b36ae5fe169eaf0f6
bcb <benjamincb@hotmail.dk>
2023-07-29 22:49:01 +0200

    add background

--- derivations/sddm-sugar-dark.nix
+++ derivations/sddm-sugar-dark.nix
@@ -1,8 +1,15 @@
 { pkgs }:
 
+let
+    imgLink = "https://gitlab.com/Ace_BCB/nixos-flake/-/raw/master/wallpapers/nix-wallpaper-nineish-dark-gray.png?ref_type=heads";
+    image = pkgs.fetchurl {
+        url = imgLink;
+        sha256 = "sha256-nhIUtCy/Hb8UbuxXeL3l3FMausjQrnjTVi1B3GkL9B8=";
+    };
+in
 pkgs.stdenv.mkDerivation {
     name = "sddm-sugar-dark";
-    src = pkgs.fetchFromGithub {
+    src = pkgs.fetchFromGitHub {
         owner = "MarianArlt";
         repo = "sddm-sugar-dark";
         rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
@@ -10,6 +17,9 @@ pkgs.stdenv.mkDerivation {
     };
     installPhase = ''
         mkdir -p $out
-        cp -R ./* $out/
+        cp -r ./* $out/
+        cd $out/
+        rm Background.jpg
+        cp -r ${image} Background.jpg
     '';
 }
