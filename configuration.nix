bdde5b7f8334ef5b7ea5af15107ca0e50b3d60f8
bcb <benjamincb@hotmail.dk>
2023-07-29 18:23:39 +0200

    rm xserver stuff

--- configuration.nix
+++ configuration.nix
@@ -60,9 +60,6 @@
     # };
     services.xserver = {
         enable = true;
-        layout = "us";
-        xkbVariant = "";
-        libinput.enable = true;
         displayManager.gdm = {
             enable = true;
             wayland = true;
