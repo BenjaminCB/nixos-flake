26444966405496b65f4e253e5e72cab886973c07
bcb <benjamincb@hotmail.dk>
2023-07-30 22:08:00 +0200

    add lf, but still no image preview

--- home.nix
+++ home.nix
@@ -75,6 +75,22 @@ in
             acpi
         ]);
 
+    programs.lf = {
+        enable = true;
+        previewer.source = pkgs.writeShellScript "pv.sh" ''
+            case "$1" in
+                *.tar*) ${pkgs.gnutar}/bin/tar tf "$1";;
+                *.zip) ${pkgs.unzip}/bin/unzip -l "$1";;
+                *.rar) ${pkgs.unrar}/bin/unrar l "$1";;
+                *.7z) ${pkgs.p7zip}/bin/7z l "$1";;
+                *.png) ${pkgs.libsixel}/bin/img2sixel "$1";;
+                *.jpg) ${pkgs.libsixel}/bin/img2sixel "$1";;
+                *.jpeg) ${pkgs.libsixel}/bin/img2sixel "$1";;
+                *) highlight -O ansi "$1" || cat "$1";;
+            esac
+        '';
+    };
+
     programs.neovim = {
         enable = true;
         withNodeJs = true;
