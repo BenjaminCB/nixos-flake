93166e6252b1c420cc3fd46fdbcc34814759209b
bcb <benjamincb@hotmail.dk>
2023-08-10 17:06:05 +0200

    try to make utility to no avail

--- derivations/dir-jumper.nix
+++ derivations/dir-jumper.nix
@@ -1,6 +1,30 @@
 { pkgs }:
 
-pkgs.writeScriptBin "dev-environments" ''
-    dir = $(dirname $0)
-    echo $dir
+pkgs.writers.writePython3Bin "dj" {} ''
+    import os
+
+
+    def current_path():
+        print("Current working directory before")
+        print(os.getcwd())
+        print()
+
+
+    current_path()
+    os.chdir('../')
+    current_path()
 ''
+
+# pkgs.writeShellApplication {
+    # name = "dj";
+    # runtimeInputs = with pkgs; [ coreutils fzf ];
+    # text = ''
+        # touch ~/.dir-jumper
+
+        # dir=$(pwd)
+        # echo "$dir" >> ~/.dir-jumper
+
+        # cdto=$(fzf < ~/.dir-jumper)
+        # cd "$cdto"
+    # '';
+# }
