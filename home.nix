9fe7923a71a4c19df01296f49a38be596e3f3b3c
bcb <benjamincb@hotmail.dk>
2023-08-28 22:01:19 +0200

    dont hide cursor when typing in wezterm

--- home.nix
+++ home.nix
@@ -74,7 +74,8 @@ in
             return {
                 font = wezterm.font("FiraCode"),
                 font_size = 18.0,
-                color_scheme = "myTheme"
+                color_scheme = "myTheme",
+                hide_mouse_cursor_when_typing = false
             }
         '';
     };
