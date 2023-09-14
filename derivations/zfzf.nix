af765a437ea49c678afcfae55f3cc3af3bd1bae0
bcb <benjamincb@hotmail.dk>
2023-09-14 23:39:23 +0200

    dont run zfzf in background

--- derivations/zfzf.nix
+++ derivations/zfzf.nix
@@ -4,6 +4,6 @@ pkgs.writeShellApplication {
     name = "zfzf";
     runtimeInputs = with pkgs; [ zathura fzf coreutils-full ];
     text = ''
-        nohup zathura "$(fzf)" &
+        nohup zathura "$(fzf)"
     '';
 }
