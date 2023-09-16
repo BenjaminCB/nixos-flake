da5ceeacfec6e3a15ce62c95c2fd99864fadde42
bcb <benjamincb@hotmail.dk>
2023-09-16 23:35:18 +0200

    rm ranger comment

--- home.nix
+++ home.nix
@@ -29,21 +29,7 @@ in
         grim slurp
 
         ### term applications
-        ncpamixer
-        gotop
-        # ranger is not working with ueberzug
-        # (ranger.overrideAttrs (r: {
-        #   preConfigure = r.preConfigure + ''
-        #     # Specify path to Überzug
-        #     substituteInPlace ranger/ext/img_display.py \
-        #       --replace "Popen(['ueberzug'" \
-        #                 "Popen(['${pkgs.ueberzug}/bin/ueberzug'"
-        #     # Use Überzug as the default method
-        #     substituteInPlace ranger/config/rc.conf \
-        #       --replace 'set preview_images_method w3m' \
-        #                 'set preview_images_method ueberzug'
-        #   '';
-        # }))
+        ncpamixer gotop
 
         ### utilities
         automake bat fd ffmpeg fzf ncdu pandoc lsix ripgrep
