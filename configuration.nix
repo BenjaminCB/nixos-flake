85680ec973ec848d14f48fb505760fb2caf5d1a0
bcb <benjamincb@hotmail.dk>
2023-09-25 21:11:17 +0200

    add secret

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,15 @@
         jack.enable = true;
     };
 
+    services.postgresql = {
+        enable = true;
+        ensureDatabases = [ "mydatabase" ];
+        authentication = pkgs.lib.mkOverride 10 ''
+            #type database  DBuser  auth-method
+            local all       all     trust
+        '';
+    };
+
     xdg.portal.enable = true;
     xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
