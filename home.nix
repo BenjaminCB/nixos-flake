e5a91dc5c0da3bab18437d7c391a290f0574aab1
bcb <benjamincb@hotmail.dk>
2023-09-29 22:47:53 +0200

    add direnv

--- home.nix
+++ home.nix
@@ -45,6 +45,7 @@ in
     programs.lf = import ./nix-dotfiles/lf.nix { inherit pkgs; };
     programs.neovim = import ./nix-dotfiles/neovim/neovim.nix { inherit pkgs secrets; };
     programs.git = import ./nix-dotfiles/git.nix;
+    programs.direnv = import ./nix-dotfiles/direnv.nix;
 
     gtk = import ./nix-dotfiles/gtk.nix { inherit pkgs; };
     qt = import ./nix-dotfiles/qt.nix { inherit pkgs; };
