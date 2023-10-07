fe8e7a8e96d8dbe0cfacce09f7279110d9f9d593
bcb <benjamincb@hotmail.dk>
2023-10-07 22:16:41 +0200

    rm ranger

--- home.nix
+++ home.nix
@@ -29,7 +29,7 @@ in
         grim slurp
 
         ### term applications
-        ncpamixer gotop chatgpt-cli ranger
+        ncpamixer gotop chatgpt-cli
 
         ### utilities
         automake bat fd ffmpeg fzf ncdu pandoc lsix ripgrep
