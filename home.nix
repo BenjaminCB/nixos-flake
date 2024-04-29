5e72b9f7e493a644063ebfcbffe6a4d55b2903a0
bcb <benjamincb@hotmail.dk>
2024-04-29 14:34:23 +0200

    update flake

--- home.nix
+++ home.nix
@@ -43,7 +43,7 @@ in
 
     programs.foot = import ./nix-dotfiles/foot.nix;
     programs.wezterm = import ./nix-dotfiles/wezterm.nix // { enable = false; };
-    programs.kitty = import ./nix-dotfiles/kitty.nix // { enable = false; };
+    programs.kitty = import ./nix-dotfiles/kitty.nix // { enable = true; };
     programs.lf = (import ./nix-dotfiles/lf.nix { inherit pkgs; }) // { enable = true; };
     programs.neovim = import ./nix-dotfiles/neovim/neovim.nix { inherit pkgs secrets; };
     programs.git = import ./nix-dotfiles/git.nix;
