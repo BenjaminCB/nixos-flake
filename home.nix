4dda3711fb201b63347e87e9c2ab8a56f33003ce
bcb <benjamincb@hotmail.dk>
2024-04-29 14:38:54 +0200

    rnix-lsp is unmaintainted

--- home.nix
+++ home.nix
@@ -38,7 +38,7 @@ in
         # nitrogen -- not wayland compatible, or maybe it is?
         wpaperd
         ### programming stuff
-        luajit gcc yarn coq_8_9 nodejs lua-language-server rnix-lsp
+        luajit gcc yarn coq_8_9 nodejs lua-language-server
     ]);
 
     programs.foot = import ./nix-dotfiles/foot.nix;
