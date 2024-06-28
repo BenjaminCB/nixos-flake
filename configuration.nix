6572dbfd1e4f298afbeb4abd12cd576c511692ab
bcb <benjamincb@hotmail.dk>
2024-06-28 22:22:50 +0200

    start firefox in wayland

--- configuration.nix
+++ configuration.nix
@@ -161,6 +161,7 @@
         EDITOR="nvim";
         # Hint electron apps to use wayland
         NIXOS_OZONE_WL = "1";
+        MOZ_ENABLE_WAYLAND = "1";
     };
 
     fonts.packages = with pkgs; [
