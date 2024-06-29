6b9a2f8eeb209b54844a262dfd470b1d31cc41e2
bcb <benjamincb@hotmail.dk>
2024-06-29 17:27:14 +0200

    do not set background in hyprland conf

--- home.nix
+++ home.nix
@@ -34,7 +34,7 @@ in
         automake bat fd ffmpeg fzf ncdu pandoc lsix ripgrep
         speedtest-cli unzip zip wmname xclip xdg-utils poppler_utils
         unrar p7zip tree-sitter acpi git-crypt kitty-themes just mermaid-cli
-        xwaylandvideobridge swaybg
+        xwaylandvideobridge
         # nitrogen -- not wayland compatible, or maybe it is?
 
         ### programming stuff
