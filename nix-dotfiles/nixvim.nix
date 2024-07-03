26caf08c627bc76524db71dd38dbc6c8fcc5cb06
bcb <benjamincb@hotmail.dk>
2024-07-03 22:56:48 +0200

    import most options

--- nix-dotfiles/nixvim.nix
+++ nix-dotfiles/nixvim.nix
@@ -1,6 +1,50 @@
 {
     enable = true;
     globals.mapleader = " ";
+    opts = {
+        number = true;
+        relativenumber = true;
+        wrap = false;
+        colorcolumn = "80";
+        shiftwidth = 4;
+        tabstop = 4;
+        expandtab = true;
+        smartindent = true;
+        splitright = true;
+        splitbelow = true;
+        softtabstop = 4;
+        wildmode = "longest,list,full";
+        ruler = true;
+        undolevels = 1000;
+        foldmethod = "marker";
+        exrc = true;
+        hlsearch = false;
+        scrolloff = 6;
+        backup = false;
+        writebackup = false;
+        updatetime = 100;
+        signcolumn = "yes";
+        termguicolors = true;
+        completeopt = "menuone,noinsert,noselect";
+        shortmess = "c";
+        cmdheight = 2;
+    };
+    clipboard = {
+        register = "unnamedplus";
+        providers.wl-copy.enable = true;
+    };
+    autoCmd = [
+        {
+            command = "setlocal wrap colorcolumn=0 spell spelllang=da,en_gb";
+            event = [ "BufEnter" "BufWinEnter" ];
+            pattern = [ "*.md" ];
+        }
+        {
+            command = "nnoremap <buffer> <leader>sc [slz=";
+            event = [ "BufEnter" "BufWinEnter" ];
+            pattern = [ "*.md" ];
+        }
+    ];
 	plugins = {
 	    lualine.enable = true;
         telescope = {
