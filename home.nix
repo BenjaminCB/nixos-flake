dcf20ea20612aa9ad9a593561b5756398826b1d3
bcb <benjamincb@hotmail.dk>
2025-08-08 18:36:30 +0200

    add some programs

--- home.nix
+++ home.nix
@@ -4,6 +4,10 @@
   home.username = "bcb";
   home.homeDirectory = "/home/bcb";
 
+  home.packages = with pkgs; [
+    neovim gotop
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
