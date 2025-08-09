1f5c7a5175b99e259b4f7e49f6b62fbefedc18bc
bcb <benjamincb@hotmail.dk>
2025-08-09 23:35:24 +0200

    zathura

--- home.nix
+++ home.nix
@@ -1,37 +1,60 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "bcb";
   home.homeDirectory = "/home/bcb";
 
   home.packages = with pkgs; [
-    firefox ungoogled-chromium mpv pcmanfm spotify alacritty discord
-    bat ffmpeg fzf pandoc speedtest-cli unzip zip xdg-utils poppler_utils
-    unrar p7zip tree-sitter acpi git-crypt just gotop
+    firefox
+    ungoogled-chromium
+    mpv
+    pcmanfm
+    spotify
+    alacritty
+    discord
+    bat
+    ffmpeg
+    fzf
+    pandoc
+    speedtest-cli
+    unzip
+    zip
+    xdg-utils
+    poppler_utils
+    unrar
+    p7zip
+    tree-sitter
+    acpi
+    git-crypt
+    just
+    gotop
   ];
 
   programs.git = import ./programs/git.nix;
   programs.kitty = import ./programs/kitty.nix;
+  programs.zathura = import ./programs/zathura.nix;
 
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "application/pdf"               = "org.pwmt.zathura.desktop";
-      "image/png"                     = "sxiv.desktop";
-      "image/jpeg"                    = "sxiv.desktop";
-      "text/html"                     = "firefox.desktop";
-      "application/x-extension-htm"   = "firefox.desktop";
-      "application/x-extension-html"  = "firefox.desktop";
+      "application/pdf" = "org.pwmt.zathura.desktop";
+      "image/png" = "sxiv.desktop";
+      "image/jpeg" = "sxiv.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
       "application/x-extension-shtml" = "firefox.desktop";
-      "application/xhtml+xml"         = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
       "application/x-extension-xhtml" = "firefox.desktop";
-      "application/x-extension-xht"   = "firefox.desktop";
-      "application/x-www-browser"     = "firefox.desktop";
-      "x-www-browser"                 = "firefox.desktop";
-      "x-scheme-handler/http"         = "firefox.desktop";
-      "x-scheme-handler/https"        = "firefox.desktop";
-      "x-scheme-handler/about"        = "firefox.desktop";
-      "x-scheme-handler/unknown"      = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+      "application/x-www-browser" = "firefox.desktop";
+      "x-www-browser" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
     };
   };
 
---
+++ programs/zathura.nix
@@ -0,0 +1,11 @@
+{
+  enable = true;
+  mappings = {
+    "<C-c>" = "recolor";
+  };
+  options = {
+    recolor = true;
+    guioptions = "";
+    font = "FiraCode 16";
+  };
+}
