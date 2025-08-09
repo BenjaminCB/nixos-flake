b541f0dcab112b2cb655a091fd5a0ee1d6be9872
bcb <benjamincb@hotmail.dk>
2025-08-09 11:14:41 +0200

    set neovim as EDITOR

--- configuration.nix
+++ configuration.nix
@@ -104,12 +104,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim
+    neovim
     fishPlugins.z
     fishPlugins.puffer
     fishPlugins.hydro
   #  wget
   ];
+
+  environment.sessionVariables.EDITOR = "nvim";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
