4010095607812a019e067e839870bdaedb5b3978
bcb <benjamincb@hotmail.dk>
2025-10-27 09:03:04 +0100

    update displayports after moving

--- programs/niri/desktop-config.nix
+++ programs/niri/desktop-config.nix
@@ -1,13 +1,13 @@
 {pkgs, ...}: let
   monitorConfig = ''
-    output "DP-3" {
+    output "DP-2" {
         mode "2560x1080@120.000"
         scale 1
         transform "normal"
         position x=-320 y=1080
     }
 
-    output "DP-2" {
+    output "DP-3" {
         mode "1920x1080@60.000"
         scale 1
         transform "normal"
