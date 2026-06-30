a4d4a9355b51311623949e33bb1c396da89cccbd
benjamincb <benjamincb@hotmail.dk>
2026-06-30 11:21:18 +0200

    ipv4 forwarding

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
   networking = {
     hostName = "nixos"; # Define your hostname.
